40d5a8a6b0b80e04f7040db3ca0f06fcafc220e4
unknown <dbrito@ci-12075-0.uolcorp.intranet>
2014-12-19 17:15:14 -0200

    Primeiro Commit

---
+++ StringToBinary.js
@@ -0,0 +1,13 @@
+function StringAndBinary(){
+	this.convert = function (str) {
+		return str.replace(/[01]{8}/g, function(v){  return String.fromCharCode( parseInt(v,2) )  });
+	}
+	this.reverse = function (str) {
+		var d,arr = [];
+		for (var i in str) {
+			d = str.charCodeAt(i).toString(2);
+			arr.push( new Array(9 - d.length).join('0') + d );
+		}
+		return arr;
+	}
+}
